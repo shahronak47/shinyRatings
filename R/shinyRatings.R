4aa664c46dcbb6e09deb832751b590149ffad1ae
Ronak Shah <shahronak47@yahoo.in>
2023-02-06 12:54:46 +0800

    remove dummy functions and docs

---
+++ R/shinyRatings.R
@@ -0,0 +1,11 @@
+#' Include ratings in shiny UI
+#'
+#' @param inputId The input slot that will be used to access the value of number of stars.
+#' @param label Display label on top of ratings stars
+#'
+#' @return Ratings to be added to UI definition
+#' @export
+#'
+shinyRatings <- function(inputId, label) {
+  
+}
