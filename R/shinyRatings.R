3dc9e5e9ab01d506c73ae8662332bc350ce03aee
Ronak Shah <shahronak47@yahoo.in>
2023-02-27 21:14:14 +0800

    first attempt of dynamic stars

--- R/shinyRatings.R
+++ R/shinyRatings.R
@@ -39,27 +39,8 @@ shinyRatings <- function(inputId) {
 
 #' @noRd
 ratings_html <- function() {
-  '
-    <div class="rating-group">
-      <label aria-label="0.5 stars" class="rating__label rating__label--half" for="rating2-05"><i class="rating__icon rating__icon--star fa fa-star-half"></i></label>
-      <input class="rating__input" name="rating2" id="rating2-05" value="0.5" type="radio">
-      <label aria-label="1 star" class="rating__label" for="rating2-10"><i class="rating__icon rating__icon--star fa fa-star"></i></label>
-      <input class="rating__input" name="rating2" id="rating2-10" value="1" type="radio">
-      <label aria-label="1.5 stars" class="rating__label rating__label--half" for="rating2-15"><i class="rating__icon rating__icon--star fa fa-star-half"></i></label>
-      <input class="rating__input" name="rating2" id="rating2-15" value="1.5" type="radio">
-      <label aria-label="2 stars" class="rating__label" for="rating2-20"><i class="rating__icon rating__icon--star fa fa-star"></i></label>
-      <input class="rating__input" name="rating2" id="rating2-20" value="2" type="radio">
-      <label aria-label="2.5 stars" class="rating__label rating__label--half" for="rating2-25"><i class="rating__icon rating__icon--star fa fa-star-half"></i></label>
-      <input class="rating__input" name="rating2" id="rating2-25" value="2.5" type="radio">
-      <label aria-label="3 stars" class="rating__label" for="rating2-30"><i class="rating__icon rating__icon--star fa fa-star"></i></label>
-      <input class="rating__input" name="rating2" id="rating2-30" value="3" type="radio">
-      <label aria-label="3.5 stars" class="rating__label rating__label--half" for="rating2-35"><i class="rating__icon rating__icon--star fa fa-star-half"></i></label>
-      <input class="rating__input" name="rating2" id="rating2-35" value="3.5" type="radio">
-      <label aria-label="4 stars" class="rating__label" for="rating2-40"><i class="rating__icon rating__icon--star fa fa-star"></i></label>
-      <input class="rating__input" name="rating2" id="rating2-40" value="4" type="radio">
-      <label aria-label="4.5 stars" class="rating__label rating__label--half" for="rating2-45"><i class="rating__icon rating__icon--star fa fa-star-half"></i></label>
-      <input class="rating__input" name="rating2" id="rating2-45" value="4.5" type="radio">
-      <label aria-label="5 stars" class="rating__label" for="rating2-50"><i class="rating__icon rating__icon--star fa fa-star"></i></label>
-      <input class="rating__input" name="rating2" id="rating2-50" value="5" type="radio">
-    </div>'
+  gen_seq <- seq(0.5, 5, 0.5)
+  seq_chr <- sub('.', '', fixed = TRUE, formatC(gen_seq, digits = 1, format = "f"))
+  dynamic_html <- paste0(sprintf('<label aria-label="%s stars" class="rating__label rating__label--half" for="rating2-%s"><i class="rating__icon rating__icon--star fa fa-star-half"></i></label><input class="rating__input" name="rating2" id="rating2-%s" value="%s" type="radio">', gen_seq, seq_chr, seq_chr, gen_seq), collapse = "")
+  sprintf('<div class="rating-group">%s</div>', dynamic_html)
 }
